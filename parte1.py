453b46d4f15bf9f3c3fd7e82a36b68b4aa75494e
GiulioCornelli <cornelli.giulio05@gmail.com>
2026-03-15 15:55:55 +0100

    aggregate functions: sum, count, min, max

--- parte1.py
+++ parte1.py
@@ -1,9 +1,26 @@
+
 import pandas as pd
 
 #import csv file 
 df = pd.read_csv('data/pokemon.csv')
-print(df)
 
-#import json file
-df = pd.read_json('data/pokemon.json')
-print(df.to_string())
+# Tutto il data frame
+# print(df.mean(numeric_only=True)) #Calcola la media aritmetica dei valori
+# print(df.sum(numeric_only=True))  #Calcola la somma totale dei valori degli elementi
+# print(df.max(numeric_only=True))  #Restituisce il valore massimo trovato in ogni colonna
+# print(df.min(numeric_only=True))  #Restituisce il valore minimo trovato in ogni colonna
+# print(df.count())                 #Conta il numero di valori non nulli in ogni colonna
+
+# una singoloa riga
+# print(df['HP'].mean())
+# print(df['HP'].sum())
+# print(df['HP'].max())
+# print(df['HP'].min())
+# print(df['HP'].count())
+
+group = df.groupby("Type 1") 
+print(group["HP"].min())
+
+
+# Altro tipo di aggregazione estraendo solo un gruppo
+print(group.get_group('Fire')['Name'].count())
