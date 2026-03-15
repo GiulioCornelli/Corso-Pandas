42c484f1bdce277e845d4f4e406acea3dff13909
GiulioCornelli <cornelli.giulio05@gmail.com>
2026-03-15 11:52:12 +0100

    test git graph

--- parte1.py
+++ parte1.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 #import csv file 
 df = pd.read_csv('data/pokemon.csv')
-print(df)
 
-#import json file
-df = pd.read_json('data/pokemon.json')
-print(df.to_string())
+# selet colum
+print(df["name"])
