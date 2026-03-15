e90c23f781f83d6e68fcf60568f5a7b2db8f0d4a
GiulioCornelli <cornelli.giulio05@gmail.com>
2026-03-15 12:23:52 +0100

    selezione tramite padas

--- parte1.py
+++ parte1.py
@@ -1,7 +1,26 @@
 import pandas as pd
 
 #import csv file 
-df = pd.read_csv('data/pokemon.csv')
+df = pd.read_csv('data/pokemon.csv', index_col=['Name'])
 
 # selet colum
-print(df["name"])
+# print(df['Type 1'])
+
+#selezione multipla
+# print(df[['Type 1','Type 2']])
+
+#selezione di una riga/righe
+print(df.loc['Charizard'])
+print(df.loc['Charmander':'Blastoise']) # da chiave a chiave
+print(df.iloc[0:11]) # gli estremi sono esclusi
+print(df.iloc[0:11:2, 0:3])# muovendosi ogni 2 e prendendo le prite 3 colonne
+
+# Esercizio
+print("---------")
+pokemon = input("Inserisci il nome del Pokemon: ")
+
+try:
+    print("Pokemon Sceloto:")
+    print(df.loc[pokemon])
+except:
+    print("Il pokemon non è presete")
